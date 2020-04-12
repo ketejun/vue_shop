28031bea8379a34fe442ede9fe528ce993b78d21
kete <ketenet@163.com>
2020-04-12 16:40:45 +0800

    completed login moudul

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Home组件
+    <div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 导入全局样式表
+import '../src/assets/css/global.css'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
