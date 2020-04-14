e23d388bfbf5e951e70833d61fc287acc37bd356
kete <ketenet@163.com>
2020-04-14 23:01:22 +0800

    完成用户列表开发

--- src/main.js
+++ src/main.js
@@ -9,6 +9,11 @@ import '../src/assets/css/global.css'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+axios.interceptors.request.use(config => {
+  // console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
